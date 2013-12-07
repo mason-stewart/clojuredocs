337f43ea5ca656bb474a8d627530d44cf64e8b9b
Mason Stewart <masondesu@gmail.com>
2013-12-06 19:22:11 -0500

    A littel refactoring, clean-up, and documentation

---
+++ src/clojuredocs/config.clj
@@ -0,0 +1,15 @@
+(ns clojuredocs.config)
+
+(def contrib-meta {:name "Clojure Contrib"
+                   :description "The user contributions library, clojure.contrib, is a collection of namespaces each of which implements features that we believe may be useful to a large part of the Clojure community."
+                   :site-url "http://richhickey.github.com/clojure-contrib/"
+                   :copyright "&copy Rich Hickey.  All rights reserved."
+                   :license "<a href=\"http://www.eclipse.org/legal/epl-v10.html\">Eclipse Public License 1.0</a>"
+                   :version "1.2.0"})
+
+(def core-meta {:name "Clojure Core"
+                :description "Clojure core environment and runtime library."
+                :site-url "http://clojure.org"
+                :copyright "&copy Rich Hickey.  All rights reserved."
+                :license "<a href=\"http://www.eclipse.org/legal/epl-v10.html\">Eclipse Public License 1.0</a>"
+                :version "1.5.1"})
--- src/clojuredocs/core.clj
+++ src/clojuredocs/core.clj
@@ -2,15 +2,17 @@
 ;; See https://github.com/zk/clojuredocs-analyzer/commits/master/src/cd_analyzer/core.clj for details on who did what :)
 
 (ns clojuredocs.core
-  (:use [clojuredocs.utils]
-	      [clojure.pprint :only (pprint)]
+  (:use [clojuredocs.config]
+        [clojuredocs.utils]
+        [clojure.pprint :only (pprint)]
         [clostache.parser])
   (:require [clojure.data.json :as json])
   (:import [java.io File FileReader]
-	         [clojure.lang LineNumberingPushbackReader]))
+           [clojure.lang LineNumberingPushbackReader]))
 
 
 (defn to-var-map [v]
+  "Turns a var into a map tailored to our specific needs."
   (let [{:keys [name ns doc line file arglists added]} (meta v)]
     {:name (str name)
      :ns (str ns)
@@ -21,61 +23,49 @@
      :source (source-for v)
      :arglists arglists
      :vars-in (map #(let [meta (meta %)]
-		      {:ns (str (:ns meta))
-		       :name (str (:name meta))}) (vars-in v))}))
+                      {:ns (str (:ns meta))
+                       :name (str (:name meta))}) (vars-in v))}))
 
 (defn parse-ns-map [root-path]
+  "Turn a namespace into a real cool map!"
   (fn [file]
     (when-let [ns (file-to-ns file)]
       (let [m (meta ns)]
-	{:name (.name ns)
-	 :file file
-	 :doc (:doc m)
-	 :web-path (.replace (.getAbsolutePath file) (.getAbsolutePath root-path) "")
-	 :vars (map to-var-map (ns-to-vars ns))}))))
+        {:name (.name ns)
+         :file file
+         :doc (:doc m)
+         :web-path (.replace (.getAbsolutePath file) (.getAbsolutePath root-path) "")
+         :vars (map to-var-map (ns-to-vars ns))}))))
 
 (defn parse-git [#^File git-root]
+  "Return a map with helpful git info."
   {:web-src-dir (git-dir-to-web-src-dir git-root)
    :commit (git-dir-to-commit git-root)
    :site-url (git-dir-to-site-url git-root)})
 
-(defn parse-clojure-core [#^File root]
-  (let [git-map (parse-git (mkfile root ".git"))
-	clojure-map {:name "Clojure Core"
-		     :description "Clojure core environment and runtime library."
-		     :site-url "http://clojure.org"
-		     :copyright "&copy Rich Hickey.  All rights reserved."
-		     :license "<a href=\"http://www.eclipse.org/legal/epl-v10.html\">Eclipse Public License 1.0</a>"
-		     :version "1.5.1"
-		     :source-root (mkfile root "src" "clj")}
-	project clojure-map
-	project (assoc project :cljs (cljs-in (:source-root project)))
-	project (assoc project :namespaces (map (parse-ns-map root) (:cljs project)))
-	library (merge {:projects [project]} git-map clojure-map)]
-    library))
 
-(defn parse-clojure-contrib [#^File root]
+(defn parse-repo [#^File root sub-dir metadata]
+  "Make a big-ass map out of an entire repo."
   (let [git-map (parse-git (mkfile root ".git"))
-	cc-map {:name "Clojure Contrib"
-			:description "The user contributions library, clojure.contrib, is a collection of namespaces each of which implements features that we believe may be useful to a large part of the Clojure community."
-			:site-url "http://richhickey.github.com/clojure-contrib/"
-			:copyright "&copy Rich Hickey.  All rights reserved."
-			:license "<a href=\"http://www.eclipse.org/legal/epl-v10.html\">Eclipse Public License 1.0</a>"
-			:version "1.2.0"
-			:source-root (mkfile root "src" "main" "clojure")}
-	project cc-map
-	project (assoc project :cljs (cljs-in (:source-root project)))
-	project (assoc project :namespaces (map (parse-ns-map root) (:cljs project)))
-	library (merge {:projects [project]} git-map cc-map)]
+        project metadata
+        project (assoc project :source-root sub-dir)
+        project (assoc project :cljs (cljs-in (:source-root project)))
+        project (assoc project :namespaces (map (parse-ns-map root) (:cljs project)))
+        library (merge {:projects [project]} git-map metadata)]
     library))
 
 (defn get-projects [lib]
+  "Getter for the projects in a repo"
   (:projects lib))
 
 (defn get-nss [lib]
+  "Getter for the namespaces in a repo"
   (reduce concat (map :namespaces (:projects lib))))
 
 (defn get-vars [lib]
+  "Get all the vars in all the namespaces in a repo.
+  Thread the lib (a map of a repo) through each of these, i.e.
+  (reduce concat (map :vars (reduce concat (map :namespaces (:projects lib)))))"
   (->> lib
        (:projects)
        (map :namespaces)
@@ -84,33 +74,50 @@
        (reduce concat)))
 
 (defn writefile [file text]
+  "Write/overwrite a file with the supplied text."
   (with-open [w (clojure.java.io/writer  file :append false)]
     (.write w text)))
 
 (defn report-on-lib [library]
+  "For each namespace, and each var in each namespace, dump the map into a
+  mustache template and write the corresponding files (and folders if necessary)."
   (let [start (System/currentTimeMillis)]
     (try
-     (let [projects (get-projects library)
-	         nss (get-nss library)
-	         vars (get-vars library)]
+      (let [projects (get-projects library)
+            nss (get-nss library)
+            vars (get-vars library)]
 
-       (doseq [p (sort-by :name projects)]
-         (doseq [ns (sort-by :name (:namespaces p))]
-           (doseq [v (sort-by :name (:vars ns))]
-             (.mkdir (java.io.File. "dist"))
-             (.mkdir (java.io.File. (str "dist/" (:name library))))
-             (.mkdir (java.io.File. (str "dist/" (:name library) "/" (:ns v))))
-             ;;(writefile (str "dist/" (:name library) (:ns v) "/" (:name v) ".json") (json/write-str v))
-             (writefile (str "dist/" (:name library) "/" (:ns v) "/" (:name v) ".html") (render-resource "templates/var.html.mustache" {:name (:name v)}))))))
-     (catch Exception e
-       (pprint "Import process failed: " e)))))
+        (doseq [p (sort-by :name projects)]
+          (doseq [ns (sort-by :name (:namespaces p))]
+            (doseq [v (sort-by :name (:vars ns))]
+              (let [folder-name (str "dist/" (:name library) "/" (:ns v) "/" (:name v))]
+                (.mkdir (java.io.File. "dist"))
+                (.mkdir (java.io.File. (str "dist/" (:name library))))
+                (.mkdir (java.io.File. (str "dist/" (:name library) "/" (:ns v))))
+                (.mkdir (java.io.File. folder-name))
 
-(defn run-update-clojure-core [root-dir]
-  (report-on-lib (parse-clojure-core (File. root-dir))))
+                ;; Check to see if the examples.html exists for this var, and if not, create it.
+                (if (#(.exists (clojure.java.io/as-file %)) (str folder-name "examples.html"))
+                  (writefile (str folder-name "/examples.html") (render-resource "templates/examples.html.mustache" v)))
 
-(defn run-update-clojure-contrib [root-dir]
-  (report-on-lib (parse-clojure-contrib (File. root-dir))))
+                ;; Do the same for see-also.html
+                (if (#(.exists (clojure.java.io/as-file %)) (str folder-name "see-also.html"))
+                  (writefile (str folder-name "/see-also.html") (render-resource "templates/see-also.html.mustache" v)))
+
+                (writefile (str folder-name "/index.html") (render-resource "templates/var.html.mustache" v))))))
+      (catch Exception e
+        (pprint "Import process failed: " e)))
+    (pprint (str "Took " (/ (- (System/currentTimeMillis) start) 1000.0) "s"))))
 
 
-(run-update-clojure-core "/Users/masondesu/clojure")
+(defn run-update-clojure-core [root-dir sub-dir]
+  (report-on-lib (parse-repo (File. root-dir) (File. (str root-dir sub-dir)) core-meta)))
+
+(defn run-update-clojure-contrib [root-dir sub-dir]
+  (report-on-lib (parse-repo (File. root-dir) (File. (str root-dir sub-dir)) contrib-meta)))
+
+
+(File. (str "/Users/masondesu/clojure/" "src/clj"))
+
+(run-update-clojure-core "/Users/masondesu/clojure/" "src/clj")
 #_(run-update-clojure-contrib "/Users/masondesu/clojurelibs/clojure-contrib")
